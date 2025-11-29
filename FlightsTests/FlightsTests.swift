4734f7aec3fa73234f66dfc055591924d7544c79
Erin <erinheim242@gmail.com>
2025-11-29 10:24:11 -0800

    Initial Commit

---
+++ FlightsTests/FlightsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlightsTests.swift
+//  FlightsTests
+//
+//  Created by Erin Heim on 11/29/25.
+//
+
+import Testing
+@testable import Flights
+
+struct FlightsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
